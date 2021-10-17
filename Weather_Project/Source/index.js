05d3a0c418d9ae4282613be5fa76db630b9a3180
kcanaan <kiaracanaan@gmail.com>
2021-10-17 17:19:48 -0400

    updated icon url protocol.

--- Weather_Project/Source/index.js
+++ Weather_Project/Source/index.js
@@ -52,7 +52,7 @@ function displayCurrentTemp(response) {
   let iconElement = document.querySelector("#icon");
   iconElement.setAttribute(
     "src",
-    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
 }
 
