e29841ed6dca7a0c4dcf3cd9a83b3565d3e74bf9
kcanaan <kiaracanaan@gmail.com>
2021-10-22 11:51:39 -0400

    added forecast with working api data

--- Weather_Project/Source/index.js
+++ Weather_Project/Source/index.js
@@ -25,6 +25,58 @@ h2.innerHTML = `${day}, ${hour}:${minute}`;
 
 //END week 4^
 //WEEK 5
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  return days[day];
+}
+function displayForecast(response) {
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 5) {
+      forecastHTML =
+        forecastHTML +
+        `
+    <div class="col-2"><span class="weekday">${formatDay(
+      forecastDay.dt
+    )}</span> 
+      <br>
+     <img
+      src="http://openweathermap.org/img/wn/${
+        forecastDay.weather[0].icon
+      }@2x.png"
+      class="weekday-icon"
+      >
+<br><span class="weekday-high"> H: ${Math.round(
+          forecastDay.temp.max
+        )}°</span> <span class="weekday-low"><br> L: ${Math.round(
+          forecastDay.temp.min
+        )}°</span>`;
+
+      forecastHTML = forecastHTML + `</div>`;
+    }
+  });
+
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  let apiKey = "40b745c14eadad7b7c4e6e4bf3b70103";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayCurrentTemp(response) {
   console.log(response.data);
 
@@ -54,6 +106,8 @@ function displayCurrentTemp(response) {
     "src",
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
